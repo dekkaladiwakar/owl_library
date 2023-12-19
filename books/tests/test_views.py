4d3fe3371a76f1748f625ff7fa90e075e9160f80
dekkaladiwakar <dekkaladiwakar@gmail.com>
2023-12-20 02:40:20 +0530

    Add testcase for books_by_author view

--- books/tests/test_views.py
+++ books/tests/test_views.py
@@ -9,50 +9,15 @@ from datetime import datetime
 import uuid
 
 
-class AvailableBooksListTest(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        test_author = Author.objects.create(name='Charlie')
-
-        cls.test_book_1 = Book.objects.create(
-            owl_id=uuid.uuid4(),
-            author=test_author, title='Tale of a Tornado- Part 1',
-            pages=235,
-            type='handmade',
-            releasedAt=datetime(2022, 12, 19, 1, 1)
-        )
-
-        cls.test_book_2 = Book.objects.create(
-            owl_id=uuid.uuid4(),
-            author=test_author, title='Tale of a Tornado- Part 2',
-            pages=235,
-            type='paperback',
-            releasedAt=datetime(2023, 12, 19, 1, 1)
-        )
-
-    def setUp(self):
-        self.client = APIClient()
-
-    def test_get_available_books(self):
-        response = self.client.get(reverse('available_books_list'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        expected_data = BookSerializer(
-            [
-                self.test_book_1,
-                self.test_book_2
-            ],
-            many=True).data
-
-        response_data = response.data
-
-        self.assertEqual(response_data, expected_data)
-
-
+# Testcases for book CRUD Operations
 class BookViewSetTestCase(APITestCase):
 
     def setUp(self):
         self.test_author = Author.objects.create(name='Charlie')
+
+        # Test data
+        # Didn't include owl_id as it will be created while using Client
+        # Using Author ID as the API accepts id instead of an instance
         self.book_data = {
             'title': 'A sniper\'s paradise',
             'author': self.test_author.id,
@@ -60,6 +25,7 @@ class BookViewSetTestCase(APITestCase):
             'type': 'paperback',
             'releasedAt': '2023-08-12T09:10:00Z'
         }
+
         self.book = Book.objects.create(
             owl_id=uuid.uuid4(),
             title='A sniper\'s paradise',
@@ -110,12 +76,81 @@ class BookViewSetTestCase(APITestCase):
         self.assertEqual(self.book.author.name, 'Charlie')
         self.assertEqual(self.book.pages, 243)
         self.assertEqual(self.book.type, 'hardcover')
+
+        # As the string doesn't contain tzinfo I'm setting it to None
+        # Or you can use 2022-08-12T09:10:00+00:00Z and remove tzinfo
         self.assertEqual(self.book.releasedAt.replace(
             tzinfo=None).isoformat() + 'Z', '2022-08-12T09:10:00Z')
 
     def test_delete_book(self):
-        # Test deleting a book
         response = self.client.delete(
             reverse('book-detail', args=[self.book.owl_id]))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Book.objects.count(), 0)
+
+
+# Testcase for AvailableBooks API
+class AvailableBooksListTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        test_author = Author.objects.create(name='Charlie')
+
+        cls.test_book_1 = Book.objects.create(
+            owl_id=uuid.uuid4(),
+            author=test_author, title='Tale of a Tornado- Part 1',
+            pages=235,
+            type='handmade',
+            releasedAt=datetime(2022, 12, 19, 1, 1)
+        )
+
+        cls.test_book_2 = Book.objects.create(
+            owl_id=uuid.uuid4(),
+            author=test_author, title='Tale of a Tornado- Part 2',
+            pages=235,
+            type='paperback',
+            releasedAt=datetime(2023, 12, 19, 1, 1)
+        )
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_available_books(self):
+        response = self.client.get(reverse('available_books_list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        expected_data = BookSerializer(
+            [
+                self.test_book_1,
+                self.test_book_2
+            ],
+            many=True).data
+
+        response_data = response.data
+
+        self.assertEqual(response_data, expected_data)
+
+
+# Testcase for BooksByAuthor API
+class BookByAuthorTestCase(APITestCase):
+    def setUp(self):
+        test_author = Author.objects.create(name='Charlie')
+
+        Book.objects.create(
+            owl_id=uuid.uuid4(),
+            title='A sniper\'s paradise',
+            author=test_author,
+            pages=234,
+            type='paperback',
+            releasedAt=datetime.now()
+        )
+
+    def test_books_by_author(self):
+        response = self.client.get(
+            reverse('books_by_author', args=['Charlie']))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('A sniper\'s paradise', response.data[0]['title'])
+
+    def test_author_not_found(self):
+        response = self.client.get(
+            reverse('books_by_author', args=['Blackbeard']))
+        self.assertEqual(response.status_code, 404)
